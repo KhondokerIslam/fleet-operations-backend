2a58baa7b7ce8152d9140c401ba59c0acde0116e
KhondokerIslam <ittehadul@revesoft.com>
2024-08-01 16:41:53 +0600

    new pojos created for full out devs

---
+++ src/main/java/com/example/BackendFleetOperations/enums/FuelType.java
@@ -0,0 +1,11 @@
+package com.example.BackendFleetOperations.enums;
+
+public enum FuelType {
+
+    BioLNG,
+    LNG,
+    HVO,
+    B20,
+    Electricity;
+
+}
--- src/main/java/com/example/BackendFleetOperations/vehicle/model/VehicleRequestData.java
+++ src/main/java/com/example/BackendFleetOperations/vehicle/model/VehicleRequestData.java
@@ -1,5 +1,7 @@
 package com.example.BackendFleetOperations.vehicle.model;
 
+import com.example.BackendFleetOperations.enums.FuelType;
+import com.example.BackendFleetOperations.enums.VehicleType;
 import lombok.Data;
 
 @Data
@@ -8,4 +10,12 @@ public class VehicleRequestData {
     private Long id;
     private String regNo;
     private String modelName;
+    private Integer vehicleSize; //In Tons
+    private Integer purchaseYear;
+    private Long purchaseCost;
+    private Long dailyMaxTravelDistance; //In km
+    private Long yearlyTravelRange; //In km
+    private FuelType fuelType;
+    private Double fuelConsumptionPerKm;
+    private VehicleType vehicleType;
 }
