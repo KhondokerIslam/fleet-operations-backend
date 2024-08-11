b9b64d3231dcacb13d303c24420edcfabaf7bc7d
KhondokerIslam <ittehadul@revesoft.com>
2024-08-11 12:04:23 +0600

    enum of vehicle type also added

--- src/main/java/com/example/BackendFleetOperations/enums/VehicleType.java
+++ src/main/java/com/example/BackendFleetOperations/enums/VehicleType.java
@@ -1,9 +1,31 @@
 package com.example.BackendFleetOperations.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum VehicleType {
 
-    Truck,
-    Car,
-    Bike;
+    Truck (0, "Truck"),
+    Car (1, "Car"),
+    Bike (2, "Bike"),
+    Pickup(3, "Pickup");
 
+    private final Integer value;
+    private final String label;
+
+    VehicleType(Integer value, String label){
+
+        this.value = value;
+        this.label = label;
+    }
+
+    private static Map<Integer,String> vehicleTypeMap = new HashMap<Integer,String>();
+
+    static {
+
+        for (VehicleType vehicleType : VehicleType.values()) {
+
+            vehicleTypeMap.put( vehicleType.value, vehicleType.label );
+        }
+    }
 }
