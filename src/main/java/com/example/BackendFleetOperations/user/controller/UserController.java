5fc7b80e99751bbf3f5d7c24fa8ff6058e2da576
KhondokerIslam <ittehadul@revesoft.com>
2024-08-14 16:38:32 +0600

    bug fixed to recieve post requests of user

--- src/main/java/com/example/BackendFleetOperations/user/controller/UserController.java
+++ src/main/java/com/example/BackendFleetOperations/user/controller/UserController.java
@@ -6,11 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin( origins = "http://localhost:3000/")
 @RestController
 @RequestMapping( "/user" )
 @Slf4j
@@ -24,6 +22,7 @@ public class UserController {
             @RequestBody UserRequestData userRequestData
     ) {
 
+        System.out.println("Recieved Data: " + userRequestData);
         try {
             userService.save(userRequestData);
             return new ResponseEntity<>( "User Successfully saved. ", HttpStatus.OK);
